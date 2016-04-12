8b44e901c7f107e99515962db30d147fd99f7ccc
Dave Moten <davidmoten@gmail.com>
2016-04-12 22:09:37 +1000

    dev

--- src/main/java/com/github/davidmoten/fsm/model/Generator.java
+++ src/main/java/com/github/davidmoten/fsm/model/Generator.java
@@ -2,7 +2,6 @@ package com.github.davidmoten.fsm.model;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.stream.Collectors;
