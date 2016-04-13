502a79c78051db47ace0c5df9f8a67c21a803401
Dave Moten <davidmoten@gmail.com>
2016-04-13 11:43:13 +1000

    refactor and split into sub-modules

--- state-machine-maven-plugin/src/main/java/com/github/davidmoten/fsm/maven/GeneratorMojo.java
+++ state-machine-maven-plugin/src/main/java/com/github/davidmoten/fsm/maven/GeneratorMojo.java
@@ -1,12 +1,43 @@
 package com.github.davidmoten.fsm.maven;
 
+import java.io.File;
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
-@Mojo(name = "generate")
+import com.github.davidmoten.fsm.model.StateMachine;
+
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GeneratorMojo extends AbstractMojo {
+
+    @Parameter(name = "supplierClass", required = true)
+    String supplierClass;
+
+    @Parameter(name = "packageName", required = true)
+    String packageName;
+
+    @Parameter(name = "outputDirectory", defaultValue = "${project.build.directory}/generated-sources/java")
+    File outputDirectory;
+
+    @SuppressWarnings("unchecked")
+    @Override
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello, world.");
+        Supplier<List<StateMachine<?>>> supplier;
+        try {
+            supplier = (Supplier<List<StateMachine<?>>>) (Class.forName(supplierClass)
+                    .newInstance());
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        List<StateMachine<?>> machines = supplier.get();
+        for (StateMachine<?> machine : machines) {
+            machine.generateClasses(outputDirectory, packageName);
+        }
+        getLog().info("generated classes in " + outputDirectory + " with package " + packageName);
     }
 }
--- state-machine-test-definition/src/main/java/com/github/davidmoten/fsm/example/StateMachines.java
+++ state-machine-test-definition/src/main/java/com/github/davidmoten/fsm/example/StateMachines.java
@@ -1,17 +1,17 @@
 package com.github.davidmoten.fsm.example;
 
-import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
 
 import com.github.davidmoten.fsm.model.State;
 import com.github.davidmoten.fsm.model.StateMachine;
 import com.github.davidmoten.fsm.runtime.Create;
 
-public class ExampleMain {
+public class StateMachines implements Supplier<List<StateMachine<?>>> {
 
-    public static void main(String[] args) {
-        File directory = new File(args[0]);
-        String pkg = args[1];
-
+    @Override
+    public List<StateMachine<?>> get() {
         StateMachine<Ship> m = StateMachine.create(Ship.class);
 
         // create states (with the event used to transition to it)
@@ -21,7 +21,9 @@ public class ExampleMain {
         State<Risky> insideRisky = m.state("Inside Risky", Risky.class);
 
         // create transitions and generate classes
-        neverOutside.initial().to(outside).to(insideNotRisky).to(insideRisky)
-                .generateClasses(directory, pkg);
+        neverOutside.initial().to(outside).to(insideNotRisky).to(insideRisky);
+        return Arrays.asList(m);
+
     }
+
 }
