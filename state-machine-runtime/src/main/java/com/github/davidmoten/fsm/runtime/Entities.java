4667f206742d2c7347149197d8bc1bc7ed61f2db
Dave Moten <davidmoten@gmail.com>
2016-04-19 22:30:35 +1000

    Entities dev

--- state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/Entities.java
+++ state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/Entities.java
@@ -4,9 +4,10 @@ import java.util.function.Function;
 
 public interface Entities<Id> {
 
-	<T> void identifier(Class<T> cls, Function<T, Id> id);
+	<T> void register(Class<T> cls, Function<T, Id> id);
 	
-	<T> T lookup(Class<T> cls, Id id);
+	<T> EntityStateMachine<T> lookup(Class<T> cls, Id id);
 	
-	
+	<T> void event(Class<T> cls, Id id, Event<?> event);
+
 }
