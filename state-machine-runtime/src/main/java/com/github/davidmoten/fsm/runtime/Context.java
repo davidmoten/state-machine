902f7e2873400ec731fcb4a95c2295bc77775287
Dave Moten <davidmoten@gmail.com>
2016-04-23 19:46:51 +1000

    add stuff

--- state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/Context.java
+++ state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/Context.java
@@ -1,9 +1,9 @@
 package com.github.davidmoten.fsm.runtime;
 
 public interface Context {
-	
-	<T> void signal(T object, Event<?> event);
-	
-	void signalToSelf(Event<?> event);
-	
+
+    <T> void signal(T object, Event<?> event);
+
+    void signalToSelf(Event<?> event);
+
 }
--- state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/EntityStateMachine.java
+++ state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/EntityStateMachine.java
@@ -1,8 +1,14 @@
 package com.github.davidmoten.fsm.runtime;
 
+import java.util.List;
+
 public interface EntityStateMachine<T> extends ObjectState<T> {
 
-	boolean transitionOccurred();
-	
-	EntityStateMachine<T> event(Event<?> event);
+    boolean transitionOccurred();
+
+    EntityStateMachine<T> event(Event<?> event);
+
+    List<Signal<T, ?>> signalsToSelf();
+
+    List<Signal<?, ?>> signalsToOther();
 }
