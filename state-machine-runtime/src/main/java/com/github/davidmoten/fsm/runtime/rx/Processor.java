4e9b679b3989df5c5ada5b3c8617d7a0bdea85a2
Dave Moten <davidmoten@gmail.com>
2016-04-23 20:57:52 +1000

    gen dev

--- state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/rx/Processor.java
+++ state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/rx/Processor.java
@@ -1,11 +1,15 @@
 package com.github.davidmoten.fsm.runtime.rx;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 import com.github.davidmoten.fsm.runtime.EntityStateMachine;
+import com.github.davidmoten.fsm.runtime.Event;
 import com.github.davidmoten.fsm.runtime.ObjectState;
 import com.github.davidmoten.fsm.runtime.Signal;
 import com.github.davidmoten.rx.Transformers;
@@ -20,52 +24,66 @@ import rx.subjects.PublishSubject;
 
 public class Processor<Id> {
 
-    private final PublishSubject<Signal<?, ?>> subject;
-    private final Func1<Object, Id> id;
-    private final Func1<Id, EntityStateMachine<?>> stateMachineCreator;
-    private final Map<Id, EntityStateMachine<?>> stateMachines = new ConcurrentHashMap<>();
+	private final PublishSubject<Signal<?, ?>> subject;
+	private final Func1<Object, Id> id;
+	private final Func1<Id, EntityStateMachine<?>> stateMachineCreator;
+	private final Map<Id, EntityStateMachine<?>> stateMachines = new ConcurrentHashMap<>();
 
-    public Processor(Func1<Object, Id> id, Func1<Id, EntityStateMachine<?>> stateMachineCreator) {
-        this.subject = PublishSubject.create();
-        this.id = id;
-        this.stateMachineCreator = stateMachineCreator;
-    }
+	public Processor(Func1<Object, Id> id, Func1<Id, EntityStateMachine<?>> stateMachineCreator) {
+		this.subject = PublishSubject.create();
+		this.id = id;
+		this.stateMachineCreator = stateMachineCreator;
+	}
 
-    public Observable<EntityStateMachine<?>> asObservable() {
-        return subject
-                //
-                .toSerialized()
-                //
-                .groupBy(id)
-                //
-                // .flatMap(g -> g.scan(stateMachineCreator.call(g.getKey()),
-                // (m, signal) -> m.event(signal.event()))
-                // .doOnNext(m -> stateMachines.put(g.getKey(), m)));
-                .flatMap(g -> g.flatMap(x -> process(g.getKey(), x)));
-    }
+	public Observable<EntityStateMachine<?>> asObservable() {
+		return subject
+				//
+				.toSerialized()
+				//
+				.groupBy(id)
+				//
+				// .flatMap(g -> g.scan(stateMachineCreator.call(g.getKey()),
+				// (m, signal) -> m.event(signal.event()))
+				// .doOnNext(m -> stateMachines.put(g.getKey(), m)));
+				.flatMap(g -> g.flatMap(x -> process(g.getKey(), x.event())));
+	}
 
-    private Observable<EntityStateMachine<?>> process(Id id, Signal<?, ?> x) {
+	private Observable<EntityStateMachine<?>> process(Id id, Event<?> x) {
 
-        Func0<Deque<Signal<?, ?>>> initialStateFactory = () -> new ArrayDeque<>();
-        Func3<Deque<Signal<?, ?>>, Signal<?, ?>, Subscriber<EntityStateMachine<?>>, Deque<Signal<?, ?>>> transition = (
-                q, signal, subscriber) -> {
-            EntityStateMachine<?> m = stateMachines.get(id).event(x.event());
-            subscriber.onNext(m);
-            return null;
-        };
-        Func2<Deque<Signal<?, ?>>, Subscriber<EntityStateMachine<?>>, Boolean> completion = (q,
-                sub) -> true;
-        return Observable.just(x)
-                .compose(Transformers.stateMachine(initialStateFactory, transition, completion));
-    }
+		Func0<Deque<Event<?>>> initialStateFactory = () -> new ArrayDeque<>();
+		Func3<Deque<Event<?>>, Event<?>, Subscriber<EntityStateMachine<?>>, Deque<Event<?>>> transition = (q,
+				ev, subscriber) -> {
+			EntityStateMachine<?> m = stateMachines.get(id);
+			q.offerFirst(ev);
+			List<Signal<?,?>> signalsToOther = new ArrayList<>();
+			Event<?> event;
+			while ((event = q.pollLast()) != null) {
+				m = m.event(event);
+				subscriber.onNext(m);
+				@SuppressWarnings("unchecked")
+				List<Event<?>> signalsToSelf = ((List<Signal<?, ?>>) (List<?>) m.signalsToSelf()).stream()
+						.map(s -> s.event()).collect(Collectors.toList());
+				for (int i = signalsToSelf.size() - 1; i >= 0; i--) {
+					q.offerLast(signalsToSelf.get(i));
+				}
+				signalsToOther.addAll(m.signalsToOther());
+			}
+			for (Signal<?,?> sig: signalsToOther) {
+				subject.onNext(sig);
+			}
+			return q;
+		};
+		Func2<Deque<Event<?>>, Subscriber<EntityStateMachine<?>>, Boolean> completion = (q, sub) -> true;
+		return Observable.just(x).compose(Transformers.stateMachine(initialStateFactory, transition, completion));
+	}
 
-    public void signal(Signal<?, ?> signal) {
-        subject.onNext(signal);
-    }
+	public void signal(Signal<?, ?> signal) {
+		subject.onNext(signal);
+	}
 
-    @SuppressWarnings("unchecked")
-    public <T> ObjectState<T> get(Id id) {
-        return (EntityStateMachine<T>) stateMachines.get(id);
-    }
+	@SuppressWarnings("unchecked")
+	public <T> ObjectState<T> get(Id id) {
+		return (EntityStateMachine<T>) stateMachines.get(id);
+	}
 
 }
