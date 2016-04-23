b5c04d8a24652e74b6c0c67cfbe6c78c74a7a020
Dave Moten <davidmoten@gmail.com>
2016-04-23 21:35:13 +1000

    rx dev

--- state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/rx/Processor.java
+++ state-machine-runtime/src/main/java/com/github/davidmoten/fsm/runtime/rx/Processor.java
@@ -6,7 +6,6 @@ import java.util.Deque;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 import com.github.davidmoten.fsm.runtime.EntityStateMachine;
 import com.github.davidmoten.fsm.runtime.Event;
