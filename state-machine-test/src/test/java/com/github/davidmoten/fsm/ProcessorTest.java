95eb4fa1cbf4c28be0696135d866ff36ea0c80aa
Dave Moten <davidmoten@gmail.com>
2016-04-26 16:57:02 +1000

    test dev

--- state-machine-test/src/test/java/com/github/davidmoten/fsm/ProcessorTest.java
+++ state-machine-test/src/test/java/com/github/davidmoten/fsm/ProcessorTest.java
@@ -57,11 +57,15 @@ public class ProcessorTest {
         // do some tests with the processor
         TestSubscriber<EntityStateMachine<?>> ts = TestSubscriber.create();
         processor.observable().doOnNext(m -> System.out.println(m.state())).subscribe(ts);
+
+        // button is pressed
         processor.signal(microwave, new ButtonPressed());
         ts.assertValueCount(1);
         assertEquals(MicrowaveStateMachine.State.COOKING, ts.getOnNextEvents().get(0).state());
         signalScheduler.advanceTimeBy(29, TimeUnit.SECONDS);
         ts.assertValueCount(1);
+
+        // cooking times out
         signalScheduler.advanceTimeBy(1, TimeUnit.SECONDS);
 
         ts.assertNoErrors();
@@ -71,10 +75,13 @@ public class ProcessorTest {
             assertEquals(MicrowaveStateMachine.State.COOKING, list.get(0).state());
             assertEquals(MicrowaveStateMachine.State.COOKING_COMPLETE, list.get(1).state());
         }
+
+        // open the door
         processor.signal(microwave, new DoorOpened());
         ts.assertValueCount(3);
         assertEquals(MicrowaveStateMachine.State.DOOR_OPEN, ts.getOnNextEvents().get(2).state());
 
+        // press the button (will not start cooking)
         processor.signal(microwave, new ButtonPressed());
         {
             // should not be a transition
@@ -84,7 +91,9 @@ public class ProcessorTest {
             assertFalse(list.get(3).transitionOccurred());
         }
 
+        // stop the subscription
         processor.onCompleted();
+
         ts.awaitTerminalEvent();
     }
 
