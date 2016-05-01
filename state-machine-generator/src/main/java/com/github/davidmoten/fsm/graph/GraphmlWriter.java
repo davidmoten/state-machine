1c86432a9613d09b433646c4d03bfa1beac6d82f
Dave Moten <davidmoten@gmail.com>
2016-05-01 21:46:57 +1000

    fix xml format

--- state-machine-generator/src/main/java/com/github/davidmoten/fsm/graph/GraphmlWriter.java
+++ state-machine-generator/src/main/java/com/github/davidmoten/fsm/graph/GraphmlWriter.java
@@ -8,7 +8,8 @@ import java.util.function.Function;
 
 public class GraphmlWriter {
 
-	public void printGraphml(PrintWriter out, Graph graph, Function<GraphNode, NodeOptions> options, boolean includeDocumentation) {
+	public void printGraphml(PrintWriter out, Graph graph, Function<GraphNode, NodeOptions> options,
+			boolean includeDocumentation) {
 
 		out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
 				+ "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"  \n"
@@ -42,25 +43,32 @@ public class GraphmlWriter {
 		String to = edge.getTo().state().name();
 		String label = edge.label();
 		out.println("    <edge source=\"" + escapeXml10(from) + "\" target=\"" + escapeXml10(to) + "\">");
-		out.println("        <data key=\"d2\">" + label + "</data>");
-		out.println("<data key=\"d3\">\n" + "        <y:PolyLineEdge>\n"
-				+ "          <y:Path sx=\"0.0\" sy=\"75.0\" tx=\"-75.0\" ty=\"-0.0\">\n"
-				+ "            <y:Point x=\"75.0\" y=\"250.0\"/>\n" + "          </y:Path>\n"
-				+ "          <y:LineStyle color=\"#000000\" type=\"line\" width=\"1.0\"/>\n"
-				+ "          <y:Arrows source=\"none\" target=\"standard\"/>\n"
-				+ "          <y:EdgeLabel alignment=\"center\" configuration=\"AutoFlippingLabel\" distance=\"2.0\" fontFamily=\"Dialog\" fontSize=\"12\" fontStyle=\"plain\" hasBackgroundColor=\"false\" hasLineColor=\"false\" height=\"17.96875\" modelName=\"custom\" preferredPlacement=\"anywhere\" ratio=\"0.5\" textColor=\"#000000\" visible=\"true\" width=\"68.822265625\" x=\"-64.4111328125\" y=\"48.0078125\">"
-				+ label + "<y:LabelModel>\n"
-				+ "              <y:SmartEdgeLabelModel autoRotationEnabled=\"false\" defaultAngle=\"0.0\" defaultDistance=\"10.0\"/>\n"
-				+ "            </y:LabelModel>\n" + "            <y:ModelParameter>\n"
-				+ "              <y:SmartEdgeLabelModelParameter angle=\"0.0\" distance=\"30.0\" distanceToCenter=\"true\" position=\"right\" ratio=\"0.5\" segment=\"0\"/>\n"
-				+ "            </y:ModelParameter>\n"
-				+ "            <y:PreferredPlacementDescriptor angle=\"0.0\" angleOffsetOnRightSide=\"0\" angleReference=\"absolute\" angleRotationOnRightSide=\"co\" distance=\"-1.0\" frozen=\"true\" placement=\"anywhere\" side=\"anywhere\" sideReference=\"relative_to_edge_flow\"/>\n"
-				+ "          </y:EdgeLabel>\n" + "          <y:BendStyle smoothed=\"false\"/>\n"
-				+ "        </y:PolyLineEdge>\n" + "      </data>");
+		out.println("      <data key=\"d2\">" + label + "</data>");
+		out.println("" //
+				+ "        <data key=\"d3\">\n" // 
+				+ "          <y:PolyLineEdge>\n" //
+				+ "            <y:Path sx=\"0.0\" sy=\"75.0\" tx=\"-75.0\" ty=\"-0.0\">\n"
+				+ "              <y:Point x=\"75.0\" y=\"250.0\"/>\n" + "          </y:Path>\n"
+				+ "            <y:LineStyle color=\"#000000\" type=\"line\" width=\"1.0\"/>\n"
+				+ "            <y:Arrows source=\"none\" target=\"standard\"/>\n"
+				+ "            <y:EdgeLabel alignment=\"center\" configuration=\"AutoFlippingLabel\" distance=\"2.0\" fontFamily=\"Dialog\" fontSize=\"12\" fontStyle=\"plain\" hasBackgroundColor=\"false\" hasLineColor=\"false\" height=\"17.96875\" modelName=\"custom\" preferredPlacement=\"anywhere\" ratio=\"0.5\" textColor=\"#000000\" visible=\"true\" width=\"68.822265625\" x=\"-64.4111328125\" y=\"48.0078125\">"
+				+ label //
+				+ "              <y:LabelModel>\n" //
+				+ "                <y:SmartEdgeLabelModel autoRotationEnabled=\"false\" defaultAngle=\"0.0\" defaultDistance=\"10.0\"/>\n"
+				+ "              </y:LabelModel>\n" //
+				+ "              <y:ModelParameter>\n" //
+				+ "                <y:SmartEdgeLabelModelParameter angle=\"0.0\" distance=\"30.0\" distanceToCenter=\"true\" position=\"right\" ratio=\"0.5\" segment=\"0\"/>\n"
+				+ "              </y:ModelParameter>\n" //
+				+ "              <y:PreferredPlacementDescriptor angle=\"0.0\" angleOffsetOnRightSide=\"0\" angleReference=\"absolute\" angleRotationOnRightSide=\"co\" distance=\"-1.0\" frozen=\"true\" placement=\"anywhere\" side=\"anywhere\" sideReference=\"relative_to_edge_flow\"/>\n"
+				+ "            </y:EdgeLabel>\n" //
+				+ "            <y:BendStyle smoothed=\"false\"/>\n" //
+				+ "          </y:PolyLineEdge>\n" //
+				+ "        </data>");
 		out.println("    </edge>");
 	}
 
-	private static void printNode(PrintWriter out, GraphNode node, Function<GraphNode, NodeOptions> options, boolean includeDocumentation) {
+	private static void printNode(PrintWriter out, GraphNode node, Function<GraphNode, NodeOptions> options,
+			boolean includeDocumentation) {
 		NodeOptions op = options.apply(node);
 		String fillColor = toHexString(op.backgroundColor);
 		String modelPosition;
@@ -80,7 +88,7 @@ public class GraphmlWriter {
 				+ "            <y:BorderStyle color=\"#000000\" type=\"line\" width=\"1.0\"/>");
 		out.println("            <y:NodeLabel modelName=\"internal\" modelPosition=\"" + modelPosition + "\">"
 				+ escapeXml10("<html><p><b>" + node.state().name() + "</b></p>"
-						+ (includeDocumentation? node.state().documentation().orElse(""):"") + "</html>")
+						+ (includeDocumentation ? node.state().documentation().orElse("") : "") + "</html>")
 				+ "</y:NodeLabel>");
 		out.println("           <y:DropShadow color=\"#B3A691\" offsetX=\"5\" offsetY=\"5\"/>");
 		out.println("        </y:ShapeNode>");
