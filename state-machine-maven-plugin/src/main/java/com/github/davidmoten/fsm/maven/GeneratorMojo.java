5c0b8c20edec160cbb03f558e5d226ee5763c8b9
Dave Moten <davidmoten@gmail.com>
2016-05-03 10:47:17 +1000

    fix default color

--- state-machine-maven-plugin/src/main/java/com/github/davidmoten/fsm/maven/GeneratorMojo.java
+++ state-machine-maven-plugin/src/main/java/com/github/davidmoten/fsm/maven/GeneratorMojo.java
@@ -19,74 +19,75 @@ import com.github.davidmoten.fsm.model.StateMachine;
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GeneratorMojo extends AbstractMojo {
 
-	@Parameter(name = "supplierClass", required = true)
-	String supplierClass;
+    @Parameter(name = "supplierClass", required = true)
+    String supplierClass;
 
-	@Parameter(name = "packageName", required = true)
-	String packageName;
+    @Parameter(name = "packageName", required = true)
+    String packageName;
 
-	@Parameter(name = "outputDirectory", defaultValue = "${project.build.directory}/generated-sources/java")
-	File outputDirectory;
+    @Parameter(name = "outputDirectory", defaultValue = "${project.build.directory}/generated-sources/java")
+    File outputDirectory;
 
-	@Parameter(name = "diagramsDirectory", defaultValue = "${project.build.directory}/state-machine-docs")
-	File diagramsDirectory;
+    @Parameter(name = "diagramsDirectory", defaultValue = "${project.build.directory}/state-machine-docs")
+    File diagramsDirectory;
 
-	@Parameter(name = "htmlDirectory", defaultValue = "${project.build.directory}/state-machine-docs")
-	File htmlDirectory;
+    @Parameter(name = "htmlDirectory", defaultValue = "${project.build.directory}/state-machine-docs")
+    File htmlDirectory;
 
-	@Parameter(name = "nodeWidth", defaultValue = "280")
-	Float nodeWidth;
+    @Parameter(name = "nodeWidth", defaultValue = "280")
+    Float nodeWidth;
 
-	@Parameter(name = "nodeHeight", defaultValue = "150")
-	Float nodeHeight;
+    @Parameter(name = "nodeHeight", defaultValue = "150")
+    Float nodeHeight;
 
-	@Parameter(name = "nodeBackgroundColor", defaultValue = "#")
-	String nodeBackgroundColor;
+    @Parameter(name = "nodeBackgroundColor", defaultValue = "#F3F2C0")
+    String nodeBackgroundColor;
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void execute() throws MojoExecutionException {
-		Supplier<List<StateMachine<?>>> supplier;
-		try {
-			supplier = (Supplier<List<StateMachine<?>>>) (Class.forName(supplierClass).newInstance());
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-		List<StateMachine<?>> machines = supplier.get();
-		for (StateMachine<?> machine : machines) {
-			machine.generateClasses(outputDirectory, packageName);
-			// with docs
-			generateGraphml(machine, true);
-			// without docs
-			generateGraphml(machine, false);
-			generateHtml(machine);
-		}
-		getLog().info("generated classes in " + outputDirectory + " with package " + packageName);
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public void execute() throws MojoExecutionException {
+        Supplier<List<StateMachine<?>>> supplier;
+        try {
+            supplier = (Supplier<List<StateMachine<?>>>) (Class.forName(supplierClass)
+                    .newInstance());
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        List<StateMachine<?>> machines = supplier.get();
+        for (StateMachine<?> machine : machines) {
+            machine.generateClasses(outputDirectory, packageName);
+            // with docs
+            generateGraphml(machine, true);
+            // without docs
+            generateGraphml(machine, false);
+            generateHtml(machine);
+        }
+        getLog().info("generated classes in " + outputDirectory + " with package " + packageName);
+    }
 
-	private void generateHtml(StateMachine<?> machine) {
-		htmlDirectory.mkdirs();
-		File gml = new File(htmlDirectory, machine.cls().getCanonicalName().replace("$", ".") + ".html");
-		try (PrintWriter out = new PrintWriter(gml)) {
-			out.println(machine.documentationHtml());
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-		getLog().info("generated graphml file for import to yed (for instance): " + gml);
+    private void generateHtml(StateMachine<?> machine) {
+        htmlDirectory.mkdirs();
+        File gml = new File(htmlDirectory,
+                machine.cls().getCanonicalName().replace("$", ".") + ".html");
+        try (PrintWriter out = new PrintWriter(gml)) {
+            out.println(machine.documentationHtml());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        getLog().info("generated graphml file for import to yed (for instance): " + gml);
 
-	}
+    }
 
-	private void generateGraphml(StateMachine<?> machine, boolean includeDocumentation) {
-		diagramsDirectory.mkdirs();
-		File gml = new File(diagramsDirectory, machine.cls().getCanonicalName().replace("$", ".")
-				+ (includeDocumentation ? "-with-docs" : "") + ".graphml");
-		try (PrintWriter out = new PrintWriter(gml)) {
-			out.println(
-					machine.graphml((node -> new NodeOptions(nodeWidth, nodeHeight, Color.decode(nodeBackgroundColor))),
-							includeDocumentation));
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-		getLog().info("generated graphml file for import to yed (for instance): " + gml);
-	}
+    private void generateGraphml(StateMachine<?> machine, boolean includeDocumentation) {
+        diagramsDirectory.mkdirs();
+        File gml = new File(diagramsDirectory, machine.cls().getCanonicalName().replace("$", ".")
+                + (includeDocumentation ? "-with-docs" : "") + ".graphml");
+        try (PrintWriter out = new PrintWriter(gml)) {
+            out.println(machine.graphml((node -> new NodeOptions(nodeWidth, nodeHeight,
+                    Color.decode(nodeBackgroundColor))), includeDocumentation));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        getLog().info("generated graphml file for import to yed (for instance): " + gml);
+    }
 }
