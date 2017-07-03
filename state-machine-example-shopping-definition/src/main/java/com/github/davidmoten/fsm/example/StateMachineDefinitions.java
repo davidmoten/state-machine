eedd5c7ae4da48a7eb02f228866866111a10c686
Dave Moten <davidmoten@gmail.com>
2017-07-03 21:08:42 +1000

    shop dev

--- state-machine-example-shopping-definition/src/main/java/com/github/davidmoten/fsm/example/StateMachineDefinitions.java
+++ state-machine-example-shopping-definition/src/main/java/com/github/davidmoten/fsm/example/StateMachineDefinitions.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Supplier;
 
 import com.github.davidmoten.fsm.example.shop.basket.Basket;
-import com.github.davidmoten.fsm.example.shop.basket.event.BasketSelectionChange;
+import com.github.davidmoten.fsm.example.shop.basket.event.Change;
 import com.github.davidmoten.fsm.example.shop.basket.event.Checkout;
 import com.github.davidmoten.fsm.example.shop.basket.event.Clear;
 import com.github.davidmoten.fsm.example.shop.basket.event.Payment;
@@ -26,13 +26,17 @@ public final class StateMachineDefinitions implements Supplier<List<StateMachine
         StateMachineDefinition<Basket> m = StateMachineDefinition.create(Basket.class);
         State<Basket, Create> created = m.createState("Created", Create.class);
         State<Basket, Clear> empty = m.createState("Empty", Clear.class);
-        State<Basket, BasketSelectionChange> changed = m.createState("Changed", BasketSelectionChange.class);
+        State<Basket, Change> changed = m.createState("Changed", Change.class);
         State<Basket, Checkout> checkedOut = m.createState("CheckedOut", Checkout.class);
         State<Basket, Payment> paid = m.createState("Paid", Payment.class);
         State<Basket, Timeout> timedOut = m.createState("TimedOut", Timeout.class);
         created.initial() //
-                .to(empty.from(changed).from(checkedOut).from(timedOut)) //
-                .to(changed) //
+                .to(empty //
+                        .from(changed) //
+                        .from(timedOut //
+                                .from(changed) //
+                                .from(checkedOut))) //
+                .to(changed.from(changed)) //
                 .to(checkedOut) //
                 .to(paid);
         return m;
--- state-machine-example-shopping-definition/src/main/java/com/github/davidmoten/fsm/example/shop/basket/event/Change.java
+++ state-machine-example-shopping-definition/src/main/java/com/github/davidmoten/fsm/example/shop/basket/event/Change.java
@@ -5,13 +5,13 @@ import java.math.BigDecimal;
 import com.github.davidmoten.fsm.example.shop.basket.Basket;
 import com.github.davidmoten.fsm.runtime.Event;
 
-public final class BasketSelectionChange implements Event<Basket>{
+public final class Change implements Event<Basket>{
 
     public final String productId;
     public final int quantity;
     public final BigDecimal unitCost;
     
-    public BasketSelectionChange(String productId, int quantity, BigDecimal unitCost) {
+    public Change(String productId, int quantity, BigDecimal unitCost) {
         this.productId = productId;
         this.quantity = quantity;
         this.unitCost = unitCost;
