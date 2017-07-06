b0b1ee5db49d0930257eba323a8fe23ed4c6c178
Dave Moten <davidmoten@gmail.com>
2017-07-06 15:37:27 +1000

    add use of Entities to unit test

--- state-machine-example-shopping/src/main/java/shop/ProductBehaviour.java
+++ state-machine-example-shopping/src/main/java/shop/ProductBehaviour.java
@@ -5,23 +5,25 @@ import com.github.davidmoten.fsm.example.generated.ProductStateMachine;
 import com.github.davidmoten.fsm.example.shop.product.Product;
 import com.github.davidmoten.fsm.example.shop.product.event.ChangeDetails;
 import com.github.davidmoten.fsm.example.shop.product.event.Create;
+import com.github.davidmoten.fsm.persistence.Entities;
 import com.github.davidmoten.fsm.runtime.Signaller;
 
 public final class ProductBehaviour extends ProductBehaviourBase<String> {
 
-	@Override
-	public ProductStateMachine<String> create(String id) {
-		return ProductStateMachine.create(id, this);
-	}
+    @Override
+    public ProductStateMachine<String> create(String id) {
+        return ProductStateMachine.create(id, this);
+    }
 
-	@Override
-	public Product onEntry_Created(Signaller<Product, String> signaller, String id, Create event, boolean replaying) {
-		return new Product(event.productId, event.name, event.description);
-	}
+    @Override
+    public Product onEntry_Created(Signaller<Product, String> signaller, String id, Create event, boolean replaying) {
+        return new Product(event.productId, event.name, event.description);
+    }
 
-	@Override
-	public Product onEntry_Changed(Signaller<Product, String> signaller, Product product, String id,
-			ChangeDetails event, boolean replaying) {
-		return new Product(product.productId, event.name, event.description);
-	}
+    @Override
+    public Product onEntry_Changed(Signaller<Product, String> signaller, Product product, String id,
+            ChangeDetails event, boolean replaying) {
+        System.out.println(Entities.get().get(Product.class) + " products found");
+        return new Product(product.productId, event.name, event.description);
+    }
 }
--- state-machine-persistence/src/test/java/com/github/davidmoten/fsm/persistence/PersistenceMicrowaveTest.java
+++ state-machine-persistence/src/test/java/com/github/davidmoten/fsm/persistence/PersistenceMicrowaveTest.java
@@ -39,172 +39,174 @@ import com.github.davidmoten.fsm.runtime.TestExecutor;
 
 public class PersistenceMicrowaveTest {
 
-	@Test
-	public void testEventSerializerRoundTrip() {
-		Serializer s = Serializer.JSON;
-		assertTrue(s.deserialize(DoorOpened.class, s.serialize(new DoorOpened())) instanceof DoorOpened);
-	}
+    @Test
+    public void testEventSerializerRoundTrip() {
+        Serializer s = Serializer.JSON;
+        assertTrue(s.deserialize(DoorOpened.class, s.serialize(new DoorOpened())) instanceof DoorOpened);
+    }
 
-	@Test
-	public void testEventSerializerRoundTripCreate() {
-		Serializer s = Serializer.JSON;
-		assertTrue(s.deserialize(Create.class, s.serialize(new Create())) instanceof Create);
-	}
+    @Test
+    public void testEventSerializerRoundTripCreate() {
+        Serializer s = Serializer.JSON;
+        assertTrue(s.deserialize(Create.class, s.serialize(new Create())) instanceof Create);
+    }
 
-	@Test
-	public void testMicrowaveSerializerRoundTrip() {
-		Serializer s = Serializer.JSON;
-		assertTrue(s.deserialize(Microwave.class, s.serialize(new Microwave(1))) instanceof Microwave);
-	}
+    @Test
+    public void testMicrowaveSerializerRoundTrip() {
+        Serializer s = Serializer.JSON;
+        assertTrue(s.deserialize(Microwave.class, s.serialize(new Microwave(1))) instanceof Microwave);
+    }
 
-	@Test
-	public void test() throws IOException {
-		MicrowaveBehaviour<String> behaviour = createMicrowaveBehaviour();
-		Function<Class<?>, EntityBehaviour<?, String>> behaviourFactory = cls -> behaviour;
-		TestExecutor executor = new TestExecutor();
-		Persistence p = createPersistence() //
-				.behaviourFactory(behaviourFactory) //
-				.executor(executor) //
-				.build();
-		p.create();
-		p.initialize();
-		assertFalse(p.get(Microwave.class, "1").isPresent());
-		assertFalse(p.getWithState(Microwave.class, "1").isPresent());
-		signal(p, new DoorOpened());
-		check(p, MicrowaveStateMachine.State.DOOR_OPEN);
-		signal(p, new DoorClosed());
-		check(p, MicrowaveStateMachine.State.READY_TO_COOK);
-		signal(p, new ButtonPressed());
-		check(p, MicrowaveStateMachine.State.COOKING);
-		executor.advance(200, TimeUnit.MILLISECONDS);
-		signal(p, new DoorOpened());
-		check(p, MicrowaveStateMachine.State.COOKING_INTERRUPTED);
-		executor.advance(2, TimeUnit.SECONDS);
-		signal(p, new DoorClosed());
-		check(p, MicrowaveStateMachine.State.READY_TO_COOK);
-		signal(p, new ButtonPressed());
-		check(p, MicrowaveStateMachine.State.COOKING);
-		executor.advance(1, TimeUnit.SECONDS);
-		check(p, MicrowaveStateMachine.State.COOKING_COMPLETE);
-		Assert.assertNotNull(p.get(Microwave.class, "1").get());
-	}
+    @Test
+    public void test() throws IOException {
+        MicrowaveBehaviour<String> behaviour = createMicrowaveBehaviour();
+        Function<Class<?>, EntityBehaviour<?, String>> behaviourFactory = cls -> behaviour;
+        TestExecutor executor = new TestExecutor();
+        Persistence p = createPersistence() //
+                .behaviourFactory(behaviourFactory) //
+                .executor(executor) //
+                .build();
+        p.create();
+        p.initialize();
+        assertFalse(p.get(Microwave.class, "1").isPresent());
+        assertFalse(p.getWithState(Microwave.class, "1").isPresent());
+        signal(p, new DoorOpened());
+        check(p, MicrowaveStateMachine.State.DOOR_OPEN);
+        signal(p, new DoorClosed());
+        check(p, MicrowaveStateMachine.State.READY_TO_COOK);
+        signal(p, new ButtonPressed());
+        check(p, MicrowaveStateMachine.State.COOKING);
+        executor.advance(200, TimeUnit.MILLISECONDS);
+        signal(p, new DoorOpened());
+        check(p, MicrowaveStateMachine.State.COOKING_INTERRUPTED);
+        executor.advance(2, TimeUnit.SECONDS);
+        signal(p, new DoorClosed());
+        check(p, MicrowaveStateMachine.State.READY_TO_COOK);
+        signal(p, new ButtonPressed());
+        check(p, MicrowaveStateMachine.State.COOKING);
+        executor.advance(1, TimeUnit.SECONDS);
+        check(p, MicrowaveStateMachine.State.COOKING_COMPLETE);
+        Assert.assertNotNull(p.get(Microwave.class, "1").get());
+    }
 
-	@Test
-	public void testRetry() throws IOException {
-		MicrowaveBehaviour<String> behaviour = createMicrowaveBehaviourThatThrows();
-		Function<Class<?>, EntityBehaviour<?, String>> behaviourFactory = cls -> behaviour;
-		TestExecutor executor = new TestExecutor();
-		List<Throwable> list = new ArrayList<>();
-		long retryIntervalMs = 5000;
-		Persistence p = createPersistence() //
-				.behaviourFactory(behaviourFactory) //
-				.executor(executor) //
-				.errorHandler(t -> list.add(t)) //
-				.retryInterval(retryIntervalMs, TimeUnit.MILLISECONDS) //
-				.build();
-		p.create();
-		p.initialize();
-		signal(p, new ButtonPressed());
-		assertFalse(p.get(Microwave.class, "1").isPresent());
-		assertEquals(1, list.size());
-		assertEquals("boo", list.get(0).getMessage());
-		executor.advance(retryIntervalMs, TimeUnit.MILLISECONDS);
-		check(p, MicrowaveStateMachine.State.COOKING);
-	}
+    @Test
+    public void testRetry() throws IOException {
+        MicrowaveBehaviour<String> behaviour = createMicrowaveBehaviourThatThrows();
+        Function<Class<?>, EntityBehaviour<?, String>> behaviourFactory = cls -> behaviour;
+        TestExecutor executor = new TestExecutor();
+        List<Throwable> list = new ArrayList<>();
+        long retryIntervalMs = 5000;
+        Persistence p = createPersistence() //
+                .behaviourFactory(behaviourFactory) //
+                .executor(executor) //
+                .errorHandler(t -> list.add(t)) //
+                .retryInterval(retryIntervalMs, TimeUnit.MILLISECONDS) //
+                .build();
+        p.create();
+        p.initialize();
+        signal(p, new ButtonPressed());
+        assertFalse(p.get(Microwave.class, "1").isPresent());
+        assertEquals(1, list.size());
+        assertEquals("boo", list.get(0).getMessage());
+        executor.advance(retryIntervalMs, TimeUnit.MILLISECONDS);
+        check(p, MicrowaveStateMachine.State.COOKING);
+    }
 
-	@Test
-	public void testGetByProperties() throws IOException {
-		MicrowaveBehaviour<String> behaviour = createMicrowaveBehaviour();
-		Function<Class<?>, EntityBehaviour<?, String>> behaviourFactory = cls -> behaviour;
-		TestExecutor executor = new TestExecutor();
-		Function<Object, Map<String, String>> propertiesFactory = new Function<Object, Map<String,String>>(){
-			@Override
-			public Map<String, String> apply(Object o) {
-				if (o instanceof Microwave) {
-					Map<String,String> map = new HashMap<String,String>();
-					map.put("colour", "white");
-					return map;
-				} else {
-					return Collections.emptyMap(); 
-				}
-			}};
-		Persistence p = createPersistence() //
-				.behaviourFactory(behaviourFactory) //
-				.propertiesFactory(propertiesFactory) //
-				.executor(executor) //
-				.build();
-		p.create();
-		p.initialize();
-		signal(p, new DoorOpened());
-		Set<EntityWithId<Microwave>> set = p.get(Microwave.class, "colour", "white");
-		assertEquals(1, set.size());
-	}
+    @Test
+    public void testGetByProperties() throws IOException {
+        MicrowaveBehaviour<String> behaviour = createMicrowaveBehaviour();
+        Function<Class<?>, EntityBehaviour<?, String>> behaviourFactory = cls -> behaviour;
+        TestExecutor executor = new TestExecutor();
+        Function<Object, Map<String, String>> propertiesFactory = new Function<Object, Map<String, String>>() {
+            @Override
+            public Map<String, String> apply(Object o) {
+                if (o instanceof Microwave) {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("colour", "white");
+                    return map;
+                } else {
+                    return Collections.emptyMap();
+                }
+            }
+        };
+        Persistence p = createPersistence() //
+                .behaviourFactory(behaviourFactory) //
+                .propertiesFactory(propertiesFactory) //
+                .executor(executor) //
+                .build();
+        p.create();
+        p.initialize();
+        signal(p, new DoorOpened());
+        Set<EntityWithId<Microwave>> set = p.get(Microwave.class, "colour", "white");
+        assertEquals(1, set.size());
+    }
 
-	private MicrowaveBehaviour<String> createMicrowaveBehaviourThatThrows() {
-		return new MicrowaveBehaviourBase<String>() {
+    private MicrowaveBehaviour<String> createMicrowaveBehaviourThatThrows() {
+        return new MicrowaveBehaviourBase<String>() {
 
-			boolean firstTime = true;
+            boolean firstTime = true;
 
-			@Override
-			public MicrowaveStateMachine<String> create(String id) {
-				return MicrowaveStateMachine.create(Microwave.fromId(id), id, this,
-						MicrowaveStateMachine.State.READY_TO_COOK);
-			}
+            @Override
+            public MicrowaveStateMachine<String> create(String id) {
+                return MicrowaveStateMachine.create(Microwave.fromId(id), id, this,
+                        MicrowaveStateMachine.State.READY_TO_COOK);
+            }
 
-			@Override
-			public Microwave onEntry_Cooking(Signaller<Microwave, String> signaller, Microwave microwave, String id,
-					ButtonPressed event, boolean replaying) {
-				if (firstTime) {
-					firstTime = false;
-					throw new RuntimeException("boo");
-				}
-				return microwave;
-			}
+            @Override
+            public Microwave onEntry_Cooking(Signaller<Microwave, String> signaller, Microwave microwave, String id,
+                    ButtonPressed event, boolean replaying) {
+                System.out.println(Entities.get().get(Microwave.class).size() + " entities found");
+                if (firstTime) {
+                    firstTime = false;
+                    throw new RuntimeException("boo");
+                }
+                return microwave;
+            }
 
-		};
-	}
+        };
+    }
 
-	private static Persistence.Builder createPersistence() throws IOException {
-		File directory = File.createTempFile("db-", "", new File("target"));
-		directory.mkdir();
-		Callable<Connection> connectionFactory = () -> DriverManager
-				.getConnection("jdbc:h2:" + directory.getAbsolutePath());
+    private static Persistence.Builder createPersistence() throws IOException {
+        File directory = File.createTempFile("db-", "", new File("target"));
+        directory.mkdir();
+        Callable<Connection> connectionFactory = () -> DriverManager
+                .getConnection("jdbc:h2:" + directory.getAbsolutePath());
 
-		return Persistence //
-				.connectionFactory(connectionFactory) //
-				.errorHandlerPrintStackTraceAndThrow() //
-				.entitySerializer(Serializer.JSON) //
-				.eventSerializer(Serializer.JSON);
+        return Persistence //
+                .connectionFactory(connectionFactory) //
+                .errorHandlerPrintStackTraceAndThrow() //
+                .entitySerializer(Serializer.JSON) //
+                .eventSerializer(Serializer.JSON);
 
-	}
+    }
 
-	private static void signal(Persistence p, Event<Microwave> event) {
-		p.signal(Signal.create(Microwave.class, "1", event));
-	}
+    private static void signal(Persistence p, Event<Microwave> event) {
+        p.signal(Signal.create(Microwave.class, "1", event));
+    }
 
-	private static void check(Persistence p, MicrowaveStateMachine.State state) {
-		assertEquals(state, p.getWithState(Microwave.class, "1").get().state);
-	}
+    private static void check(Persistence p, MicrowaveStateMachine.State state) {
+        assertEquals(state, p.getWithState(Microwave.class, "1").get().state);
+    }
 
-	private static MicrowaveBehaviour<String> createMicrowaveBehaviour() {
-		return new MicrowaveBehaviourBase<String>() {
+    private static MicrowaveBehaviour<String> createMicrowaveBehaviour() {
+        return new MicrowaveBehaviourBase<String>() {
 
-			@Override
-			public MicrowaveStateMachine<String> create(String id) {
-				return MicrowaveStateMachine.create(Microwave.fromId(id), id, this,
-						MicrowaveStateMachine.State.READY_TO_COOK);
-			}
+            @Override
+            public MicrowaveStateMachine<String> create(String id) {
+                return MicrowaveStateMachine.create(Microwave.fromId(id), id, this,
+                        MicrowaveStateMachine.State.READY_TO_COOK);
+            }
 
-			@Override
-			public Microwave onEntry_Cooking(Signaller<Microwave, String> signaller, Microwave microwave, String id,
-					ButtonPressed event, boolean replaying) {
-				if (!replaying) {
-					signaller.signalToSelf(new TimerTimesOut(), 1, TimeUnit.SECONDS);
-				}
-				return microwave;
-			}
+            @Override
+            public Microwave onEntry_Cooking(Signaller<Microwave, String> signaller, Microwave microwave, String id,
+                    ButtonPressed event, boolean replaying) {
+                if (!replaying) {
+                    signaller.signalToSelf(new TimerTimesOut(), 1, TimeUnit.SECONDS);
+                }
+                return microwave;
+            }
 
-		};
-	}
+        };
+    }
 
 }
