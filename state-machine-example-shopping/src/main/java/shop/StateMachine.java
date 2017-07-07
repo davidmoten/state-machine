d59eef5bee96593ada776e2ef989b767c67d371a
Dave Moten <davidmoten@gmail.com>
2017-07-07 15:45:40 +1000

    persistence dev

--- state-machine-example-shopping/src/main/java/shop/StateMachine.java
+++ state-machine-example-shopping/src/main/java/shop/StateMachine.java
@@ -1,24 +1,31 @@
 package shop;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 import com.github.davidmoten.fsm.example.shop.catalog.Catalog;
 import com.github.davidmoten.fsm.example.shop.catalog.event.Change;
 import com.github.davidmoten.fsm.example.shop.catalogproduct.CatalogProduct;
 import com.github.davidmoten.fsm.example.shop.product.Product;
-import com.github.davidmoten.fsm.example.shop.product.event.ChangeDetails;
 import com.github.davidmoten.fsm.example.shop.product.event.Create;
 import com.github.davidmoten.fsm.persistence.Persistence;
 import com.github.davidmoten.fsm.persistence.Property;
-import com.github.davidmoten.guavamini.Lists;
 
 import shop.behaviour.CatalogBehaviour;
 import shop.behaviour.CatalogProductBehaviour;
 import shop.behaviour.ProductBehaviour;
 
 public final class StateMachine {
+
+    private static final String MAIN_CATALOG_ID = "1";
 
     public static Persistence createPersistence(Callable<Connection> connectionFactory) {
         return Persistence //
@@ -37,16 +44,33 @@ public final class StateMachine {
     public static void setup(Persistence p) {
         p.create();
         p.initialize();
-        p.signal(Product.class, "12", //
-                new Create("12", "Castelli Senza Jacket", "Fleece lined windproof cycling jacket",
-                        Collections.emptyList()));
-        p.signal(Catalog.class, "1",
-                new com.github.davidmoten.fsm.example.shop.catalog.event.Create("1", "Online bike shop"));
-        p.signal(Catalog.class, "1", new Change("12", 3));
-        p.signal(Catalog.class, "1", new Change("12", 2));
-        p.signal(Product.class, "12",
-                new ChangeDetails("Castelli Senza 2 Jacket",
-                        "Fleece lined windproof cycling jacket with reflective highlights",
-                        Lists.newArrayList("Clothing", "Cycling", "Windproof", "Jacket", "Castelli")));
+        p.signal(Catalog.class, MAIN_CATALOG_ID,
+                new com.github.davidmoten.fsm.example.shop.catalog.event.Create(MAIN_CATALOG_ID, "Online bike shop"));
+        p.signal(Catalog.class, MAIN_CATALOG_ID, new Change("12", 3));
+        p.signal(Catalog.class, MAIN_CATALOG_ID, new Change("12", 2));
+        InputStreamReader in = new InputStreamReader(StateMachine.class.getResourceAsStream("/products.txt"));
+        try {
+            for (CSVRecord record : CSVFormat.DEFAULT.parse(in)) {
+                String productId = record.get(0).trim();
+                String name = record.get(1).trim();
+                String description = record.get(2).trim();
+                List<String> tags = Arrays.asList(record.get(3).split("\\|")).stream().map(x -> x.trim())
+                        .collect(Collectors.toList());
+                int quantity = Integer.parseInt(record.get(4).trim());
+                p.signal(Product.class, //
+                        productId, //
+                        new Create(productId, name, description, tags));
+                p.signal(Catalog.class, //
+                        MAIN_CATALOG_ID, new Change(productId, quantity));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // p.signal(Product.class, "12",
+        // new ChangeDetails("Castelli Senza 2 Jacket",
+        // "Fleece lined windproof cycling jacket with reflective highlights",
+        // Lists.newArrayList("Clothing", "Cycling", "Windproof", "Jacket",
+        // "Castelli")));
     }
+
 }
