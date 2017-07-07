1d2d1094b71a927384fe1035538195eff9addd9d
Dave Moten <davidmoten@gmail.com>
2017-07-07 18:08:35 +1000

    add ui

--- state-machine-example-shopping/src/main/java/shop/ShopController.java
+++ state-machine-example-shopping/src/main/java/shop/ShopController.java
@@ -1,16 +1,32 @@
 package shop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.davidmoten.fsm.example.shop.catalog.Catalog;
+import com.github.davidmoten.fsm.example.shop.catalogproduct.CatalogProduct;
+import com.github.davidmoten.fsm.persistence.Property;
 
 @Controller
 public class ShopController {
 
+    @Autowired
+    private PersistenceService persistence;
+
     @RequestMapping("/catalogs")
     public String catalog(Model model) {
-        // model.addAttribute("catalogId", name);
+        model.addAttribute("catalogs", persistence.get().get(Catalog.class));
         return "catalogs";
+    }
+
+    @RequestMapping("/catalogs/{catalogId}/products")
+    public String catalogProducts(@PathVariable("catalogId") String catalogId, Model model) {
+        model.addAttribute("catalogProducts", persistence.get() //
+                .getOr(CatalogProduct.class, Property.list("catalogId", catalogId)));
+        return "catalogProducts";
     }
 
 }
